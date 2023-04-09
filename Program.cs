08b7c7e7c3f2969066dca8138c154350cfcfe569
Naturalson001 <123705243+Naturalson001@users.noreply.github.com>
2023-04-09 19:24:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Diagnostics.Metrics;
+
+namespace Task_Range;
+class Program
+{
+    public static void Main()
+    {
+
+        Console.WriteLine("Enter a number:");
+        var N = int.Parse (Console.ReadLine());
+        Console.WriteLine(Inputs(N));
+
+        static object Inputs(int N)
+        {
+            for (int i = 1; i <= N; i++)
+            {
+
+                if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+
+
+                }
+
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+
+                }
+
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+
+                }
+                else
+                {
+                    Console.WriteLine(i);
+
+                }
+
+
+                
+              
+            }
+            Console.Read();
+            return true ;
+           
+        }
+
+        
+
+
+    }
+}
+
+
