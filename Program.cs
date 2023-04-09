f0aae63a02f6e50846b710edb48ec539b8d21f79
Naturalson001 <123705243+Naturalson001@users.noreply.github.com>
2023-04-09 19:25:36 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,39 +20,27 @@ class Program
                 if ((i % 3 == 0) && (i % 5 == 0))
                 {
                     Console.WriteLine("FizzBuzz");
-
-
                 }
 
                 else if (i % 3 == 0)
                 {
                     Console.WriteLine("Fizz");
-
                 }
 
                 else if (i % 5 == 0)
                 {
                     Console.WriteLine("Buzz");
-
                 }
                 else
                 {
                     Console.WriteLine(i);
-
                 }
-
-
-                
               
             }
             Console.Read();
             return true ;
            
         }
-
-        
-
-
     }
 }
 
